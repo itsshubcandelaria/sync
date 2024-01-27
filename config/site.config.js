563f80352fae806cb0c85ce37f9adfeb3127b1a1
Sync <SyncXII@users.noreply.github.com>
2024-01-27 11:22:58 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -49,7 +49,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'Link',
+      name: 'Wordpress',
       link: 'https://candelaria.gov.ph',
     },
   ],
